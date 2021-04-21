793d837dbc6f6bba2ae97b50db9936c57fc3c923
vlajksanyee <vlajksanyi@gmail.com>
2021-04-21 13:50:47 +0200

    Commit #4
    Information about storage

--- hardwareMonitor/hardwareMonitor/MainWindow.xaml.cs
+++ hardwareMonitor/hardwareMonitor/MainWindow.xaml.cs
@@ -26,6 +26,7 @@ namespace hardwareMonitor
 		Computer computer = new Computer() { CPUEnabled = true };
 		string[] SizeSuffixes = { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
 		List<RamClass> ramDevices = new List<RamClass>();
+		List<StorageClass> storageDevices = new List<StorageClass>();
 		public MainWindow()
 		{
 			InitializeComponent();
@@ -39,10 +40,10 @@ namespace hardwareMonitor
 			if (value < 0) { return "-" + SizeSuffix(-value); }
 			if (value == 0) { return "0.0 bytes"; }
 
-			int mag = (int)Math.Log(value, 1024);
-			decimal adjustedSize = (decimal)value / (1L << (mag * 10));
+			int ss = (int)Math.Log(value, 1024);
+			decimal adjustedSize = (decimal)value / (1L << (ss * 10));
 
-			return string.Format("{0:n1} {1}", adjustedSize, SizeSuffixes[mag]);
+			return string.Format("{0:n1} {1}", adjustedSize, SizeSuffixes[ss]);
 		}
 
 		public void Init()
@@ -92,6 +93,16 @@ namespace hardwareMonitor
 			}
 
 			//Get Storage Info
+			ManagementObjectSearcher mosStorage = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive");
+			foreach (var item in mosStorage.Get())
+			{
+				if (item != null)
+					storageDevices.Add(new StorageClass(item["Model"].ToString(), SizeSuffix(Convert.ToInt64(item["Size"])).ToString(), item["Partitions"].ToString(), item["Status"].ToString()));
+			}
+			foreach (var item in storageDevices)
+			{
+				storageCB.Items.Add(item.model);
+			}
 		}
 
 		public void SensorsCPU()
@@ -139,16 +150,25 @@ namespace hardwareMonitor
 		private void MemoryCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
 			RamClass selectRam = ramDevices.Where(x => x.tag == memoryCB.SelectedItem.ToString()).First();
-			memoryTag.Content = $"{selectRam.tag}";
-			memoryCapacity.Content = $"{selectRam.capacity}";
+			memoryTag.Content = selectRam.tag;
+			memoryCapacity.Content = selectRam.capacity;
 			if (selectRam.memoryType == "20")
-				memoryType.Content = $"DDR";
+				memoryType.Content = "DDR";
 			else if (selectRam.memoryType == "21")
-				memoryType.Content = $"DDR2";
+				memoryType.Content = "DDR2";
 			else if (selectRam.memoryType == "24")
-				memoryType.Content = $"DDR3";
+				memoryType.Content = "DDR3";
 			else if (selectRam.memoryType == "26")
-				memoryType.Content = $"DDR4";
+				memoryType.Content = "DDR4";
+		}
+
+		private void StorageCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			StorageClass selectStorage = storageDevices.Where(x => x.model == storageCB.SelectedItem.ToString()).First();
+			storageModel.Content = selectStorage.model;
+			storageSize.Content = selectStorage.size;
+			storagePartitions.Content = selectStorage.partitions;
+			storageStatus.Content = selectStorage.status;
 		}
 	}
 }
---
+++ hardwareMonitor/hardwareMonitor/storageClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hardwareMonitor
+{
+	public class StorageClass
+	{
+		public string model { get; set; }
+		public string size { get; set; }
+		public string partitions { get; set; }
+		public string status { get; set; }
+		public StorageClass(string m, string s, string p, string st)
+		{
+			this.model = m;
+			this.size = s;
+			this.partitions = p;
+			this.status = st;
+		}
+	}
+}
